5cac300be1df4b86c1c3b8cf80aba1f2711945e5
Telldrasil <Telldrasilo@gmail.com>
2013-04-13 16:22:08 +0400

    ForHer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForUbuntu::Application.config.secret_token = '24682aeb4643a20b4a72bdef6e748a58f1504011d11c50615a53eceb36214551a3893f8d7743303b4aedca88d5c0b2d28de524081090e72750620b4d7b5557a2'
